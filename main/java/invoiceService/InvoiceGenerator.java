9b509f6ab7f4ca4f46e990f7249c0a034d92493a
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-22 19:42:07 +0530

    Added code: Ride Repository

--- main/java/invoiceService/InvoiceGenerator.java
+++ main/java/invoiceService/InvoiceGenerator.java
@@ -1,9 +1,15 @@
 package invoiceService;
 
+import java.util.HashMap;
+
 public class InvoiceGenerator {
     private static final double COST_PER_KILOMETER = 10.0;
     private static final int COST_PER_MINUTE = 1;
     private static final double MIN_FARE = 5.0;
+    private static HashMap<String, Ride[]> rideRepository;
+    public InvoiceGenerator(){
+        rideRepository = new HashMap<>();
+    }
 
     public double findTotalFare(double dis, int time) {
         double fare= dis*COST_PER_KILOMETER + time*COST_PER_MINUTE;
@@ -17,5 +23,14 @@ public class InvoiceGenerator {
         }
         double avgFare = (totalFare)/ rides.length;
         return new InvoiceSummary(rides.length,totalFare, avgFare);
+    }
+
+    public void addRide(String userId, Ride[] rides) {
+        rideRepository.put(userId,rides);
+    }
+
+    public InvoiceSummary findTotalFare(String userId) {
+        Ride[] rides = rideRepository.get(userId);
+        return this.findTotalFare(rides);
     }
 }
--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -27,8 +27,23 @@ public class InvoiceServiceTest {
     }
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary(){
-        Ride[] rides={new Ride(20.0,10), new Ride(0.2,2)};
+        Ride[] rides={
+                new Ride(20.0,10),
+                new Ride(0.2,2)
+        };
         InvoiceSummary invoiceSummary = invoiceGenerator.findTotalFare(rides);
         Assert.assertEquals(new InvoiceSummary(2,215,107.5),invoiceSummary);
+    }
+    @Test
+    public void givenUserID_ShouldReturnInvoiceSummary(){
+        String userId = "U001";
+        Ride[] rides={
+                new Ride(20.0,10),
+                new Ride(0.2,2)
+        };
+        invoiceGenerator.addRide(userId,rides);
+        InvoiceSummary invoiceSummary = invoiceGenerator.findTotalFare(userId);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,215,107.5);
+        Assert.assertEquals(expectedInvoiceSummary,invoiceSummary);
     }
 }
